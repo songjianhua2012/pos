4b852cd165d91643fc3ee5efdb3dbcc2727d6850
songjianhua2012 <songjianhua2012@outlook.com>
2015-07-05 09:40:29 +0800

    pos_v0 has finished

---
+++ pos_v0/main/main.js
@@ -0,0 +1,16 @@
+//TODO: Please write code in this file.
+function printReceipt(inputs) {
+  var total = 0;
+  var str = '***<没钱赚商店>收据***\n';
+  for(var i=0; i<inputs.length; ++i)
+  {
+    str += '名称：'+inputs[i].name+'，数量：'+inputs[i].count+
+    inputs[i].unit+'，单价：'+inputs[i].price+'.00(元)'+'，小计：'+
+    inputs[i].count*inputs[i].price+'.00(元)'+'\n';
+    total += (inputs[i].count*inputs[i].price);
+  }
+  str +='----------------------'+'\n';
+  str +='总计：'+total+'.00(元)'+'\n';
+  str +='**********************';
+  console.log(str);
+}
