911ae4d4c7e041a1512a3ffd6a35f21bd038f3a8
songjianhua2012 <songjianhua2012@outlook.com>
2015-07-08 10:42:14 +0800

    pos_v0.1 finished

--- pos_v0.1/main/main.js
+++ pos_v0.1/main/main.js
@@ -1,7 +1,7 @@
 //TODO: Please write code in this file.
 function printReceipt(inputs) {
   var total_product = [];
-  for(var x=0; x<inputs.length; ++x) {
+  for(var x = 0; x < inputs.length; x++) {
     finProduct(inputs[x],total_product);
   }
 
@@ -9,7 +9,7 @@ function printReceipt(inputs) {
 }
 
 function finProduct(product,total_product) {
-  for(var y=0; y<total_product.length; ++y) {
+  for(var y = 0; y < total_product.length; y++) {
     if(product.name === total_product[y].name) {
       total_product[y].count += 1;
       return;
@@ -22,15 +22,16 @@ function finalPrint(total_product){
   var total = 0;
   var str = '***<没钱赚商店>收据***\n';
 
-  for(var i=0; i<total_product.length; ++i) {
+  for(var i = 0; i < total_product.length; i++) {
     str += '名称：'+total_product[i].name+'，数量：'+total_product[i].count+
     total_product[i].unit+'，单价：'+total_product[i].price+'.00(元)'+'，小计：'+
     total_product[i].count*total_product[i].price+'.00(元)'+'\n';
-    total += (total_product[i].count*total_product[i].price);
+
+    total += (total_product[i].count * total_product[i].price);
   }
-  
-  str +='----------------------'+'\n';
-  str +='总计：'+total+'.00(元)'+'\n';
-  str +='**********************';
+
+  str += '----------------------'+'\n';
+  str += '总计：'+total+'.00(元)'+'\n';
+  str += '**********************';
   console.log(str);
 }
